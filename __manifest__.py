2093c8a50a924d8c995aa2eeccb2a89dd4e1aeba
Mohamed Elbanna <moelbanna880@gmail.com>
2025-11-19 13:50:13 +0200

    basic module structure

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Project Cost Estimate Module",
+    "description" : "A custom module integrates project module with cost estimation features.",
+    "version" : "18.0.1.0.0",
+    "author" : "Mohamed Elbanna",
+    "data" : [],
+    "depends" : ["base", "project" , "mail"],
+    "assets" : {
+        "web.assets_backend" : [],
+        "web.assets_qweb" : [],
+    },
+    "application" : True,
+}
