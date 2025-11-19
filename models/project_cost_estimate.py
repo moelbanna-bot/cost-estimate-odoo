2aa83541c8e6f09eb5114d68d05f896695831a4b
Mohamed Elbanna <moelbanna880@gmail.com>
2025-11-19 13:58:44 +0200

    create project cost estimate model

---
+++ models/project_cost_estimate.py
@@ -0,0 +1,12 @@
+from odoo import models,fields,api
+
+
+class ProjectCostEstimate(models.Model):
+    _name = "project.cost.estimate"
+    _description = "Project Cost Estimate Model"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+
+    name = fields.Char("Cost Estimate Name" , required=True)
+    project_id = fields.Many2one("project.project" , "Linked Project" , required=True)
+    breakdown_ids = fields.One2many("project.cost.breakdown" , "estimate_id" , "Cost Breakdowns")
+    estimated_total_cost = fields.Float("Estimated Total Cost" , compute="_compute_total_cost")
